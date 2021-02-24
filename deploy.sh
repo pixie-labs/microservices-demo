4e6d704c1483ca4a76d68cf4f4f3c6e4be23d171
Yaxiong Zhao <yzhao@pixielabs.ai>
2021-02-23 18:09:48 -0800

    Change deploy with yaml file in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
-# Used for deploy online boutique for Pixie demo
-
-namespace="px-online-boutique"
-repo="gcr.io/pl-dev-infra/demos/microservices-demo"
+namespace="px-online-boutique2"
 
 kubectl create namespace ${namespace}
-skaffold run --default-repo=${repo} --namespace=${namespace} --tag=${USER}
-kubectl port-forward svc/frontend 8080:80 -n ${namespace}
+kubectl apply --filename=release/kubernetes-manifests.yaml --namespace=${namespace}
+# Uncomment the following are to use skaffold.
+# repo="gcr.io/pl-dev-infra/demos/microservices-demo"
+# skaffold run --default-repo=${repo} --namespace=${namespace}
+
+kubectl port-forward svc/frontend 8080:80 --namespace=${namespace}
