26d2159789cb90b1f6d2dfb7f1f1a6647fc4a8f1
Yaxiong Zhao <yzhao@pixielabs.ai>
2021-02-26 12:57:15 -0800

    Add redis sharding yaml

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,35 @@
-namespace="px-online-boutique2"
+#!/bin/bash
 
-kubectl create namespace ${namespace}
-kubectl apply --filename=release/kubernetes-manifests.yaml --namespace=${namespace}
+manifests_yaml=release/kubernetes-manifests.yaml
+redis_sharding=false
+if [[ "$1" == "--redis-sharding" ]]; then
+  redis_sharding=true
+  manifests_yaml=release/kubernetes-redis-sharding-manifests.yaml
+fi
+
+echo "manifests_yaml: ${manifests_yaml}"
+namespace="px-online-boutique"
+
+kubectl create namespace "${namespace}"
+kubectl apply --filename="${manifests_yaml}" --namespace="${namespace}"
+
 # Uncomment the following are to use skaffold.
 # repo="gcr.io/pl-dev-infra/demos/microservices-demo"
-# skaffold run --default-repo=${repo} --namespace=${namespace}
+# skaffold run --default-repo="${repo}" --namespace="${namespace}"
 
-kubectl port-forward svc/frontend 8080:80 --namespace=${namespace}
+
+if [[ "$redis_sharding" == true ]]; then
+  echo "Wait for Redis shard pods to be ready ..."
+  timeout=120s
+  kubectl rollout status --watch --timeout="${timeout}" --namespace="${namespace}" statefulset/redis-cart
+  if [[ $? != 0 ]]; then
+    echo "Redis shards do not come ready after ${timeout}, exit ..."
+    exit 1
+  fi
+  redis_cart_ips=$(kubectl get pods --namespace="$namespace" -l app=redis-cart \
+                   -o jsonpath='{range.items[*]}{.status.podIP}:6379 ')
+  kubectl exec --namespace="${namespace}" -it redis-cart-0 -- redis-cli --cluster create \
+    --cluster-replicas 1 ${redis_cart_ips} --cluster-yes
+fi
+
+kubectl port-forward svc/frontend 8080:80 --namespace="${namespace}"
